bd77e8403a9ffb1d254e3e3d2e5fa1e82e486877
Vinod <vinodjagwani@gmail.com>
2015-09-15 13:21:12 +0200

    Update ProductsellingfrontStandalone.java

--- src/de/hybris/platform/productsellingfront/ProductsellingfrontStandalone.java
+++ src/de/hybris/platform/productsellingfront/ProductsellingfrontStandalone.java
@@ -6,15 +6,6 @@ import de.hybris.platform.util.RedeployUtilities;
 import de.hybris.platform.util.Utilities;
 
 
-/**
- * Demonstration of how to write a standalone application that can be run directly from within eclipse or from the
- * commandline.<br>
- * To run this from commandline, just use the following command:<br>
- * <code>
- * java -jar bootstrap/bin/ybootstrap.jar "new de.hybris.platform.productsellingfront.ProductsellingfrontStandalone().run();"
- * </code> From eclipse, just run as Java Application. Note that you maybe need to add all other projects like
- * ext-commerce, ext-pim to the Launch configuration classpath.
- */
 public class ProductsellingfrontStandalone
 {
 	/**
